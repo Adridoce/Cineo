76da36ae96c9eefd48b7c3ba9d97b21b5066ddca
Adridoce <adrian.perezurrialde@gmail.com>
2025-11-10 22:12:09 +0100

    Agregado AndroidYoutubePlayer

--- app/src/main/java/com/adridoce/cineo/data/response/MovieListResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.adridoce.cineo.data.response
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class MovieListResponse(
-    val results: List<MovieResponse>
-)
--- app/src/main/java/com/adridoce/cineo/presentation/core/components/NavItem.kt
+++
@@ -1,9 +0,0 @@
-package com.adridoce.cineo.presentation.core.components
-
-import android.graphics.drawable.Icon
-import androidx.compose.ui.graphics.vector.ImageVector
-
-data class NavItem(
-    val icon: ImageVector,
-    val label: String
-)
